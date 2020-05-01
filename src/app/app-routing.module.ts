c3c4179aff5ebac30885e954ec6a15ea0b7e9e9b
Tam Pham <tampt95@gmail.com>
2020-05-01 15:59:25 +0700

    init layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,31 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AdminPageComponent} from './pages/admin-page/admin-page.component';
+import {LoginPageComponent} from './pages/login-page/login-page.component';
+import {Error404PageComponent} from './pages/error404-page/error404-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AdminPageComponent,
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent,
+  },
+  {
+    path: '404',
+    component: Error404PageComponent
+  },
+  {
+    path: '**', component: Error404PageComponent, pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LoginPageComponent} from './pages/login-page/login-page.component';
+import {Error404PageComponent} from './pages/error404-page/error404-page.component';
+import {AdminPageComponent} from './pages/admin-page/admin-page.component';
 
 @NgModule({
   declarations: [
+    LoginPageComponent,
+    Error404PageComponent,
+    AdminPageComponent,
     AppComponent
   ],
   imports: [
